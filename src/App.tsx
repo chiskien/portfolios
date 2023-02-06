25b2f615f5c02aa4b9e36a796d39b2b8ca13649c
ChisKien <chiskien03214@gmail.com>
2023-02-06 10:11:04 +0700

    Background color and reset css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
-import reactLogo from './assets/react.svg'
 import './App.css'
-import {Logo} from "./components/Logo";
-import {Card} from "./components/Card";
-import {Slogan} from "./components/Slogan";
+import {Nav} from "./components/Nav";
 
 function App() {
 
     return (
         <div className="App">
-            <Logo logo={reactLogo}/>
-            <Slogan text={`Vite and React`}/>
-            <Card/>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            <Nav/>
         </div>
     )
 }
