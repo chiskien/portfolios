6223c06322b2c34057d384de1c2254a6d7e59809
ChisKien <chiskien03214@gmail.com>
2023-02-06 10:45:01 +0700

    Nav styling with transition

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,20 @@
 import React from "react";
+import "./Nav.css"
 
 export function Nav() {
     return (
-        <div className={"nav"}>
+        <div className="nav">
             <h1>Navigation</h1>
-            <div className="nav-container">
+            <div className="nav-bar">
                 <div className="logo">ChisKien</div>
                 <div className="menu-toggle">
-                      <div className="hambuger-box">
-                          <span className="line-top"></span>
-                          <span className="line-bottom"></span>
-                      </div>
+                    <div className="ham-box">
+                        <span className="line-top"></span>
+                        <span className="line-bottom"></span>
+                    </div>
                 </div>
                 <div className="nav-overlay">
-                    
+
                 </div>
             </div>
         </div>
