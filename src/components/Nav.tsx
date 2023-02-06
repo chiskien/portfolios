ea3d23bd9b540739ed7196ea283763a971bc9d76
ChisKien <chiskien03214@gmail.com>
2023-02-06 10:55:18 +0700

    using react hook useState to handle button click

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Nav.css"
 
 export function Nav() {
+    const [navOpen, setNavOpen] = useState(false);
+
     return (
         <div className="nav">
             <h1>Navigation</h1>
-            <div className="nav-bar">
-                <div className="logo">ChisKien</div>
-                <div className="menu-toggle">
-                    <div className="ham-box">
-                        <span className="line-top"></span>
-                        <span className="line-bottom"></span>
+            <div className="nav-container">
+                <div className="nav-bar">
+                    <div className="logo">ChisKien</div>
+                    <div className="menu-toggle" onClick={() => {
+                        setNavOpen(!navOpen)
+                    }}>
+                        <div className={navOpen ? "ham-box ham-box-open" : "ham-box"}>
+                            <span className={navOpen ? "line-top spin" : "line-top"}></span>
+                            <span className={navOpen ? "line-bottom spin" : "line-bottom"}></span>
+                        </div>
                     </div>
-                </div>
-                <div className="nav-overlay">
+                    <div className="nav-overlay" style={{
+                        top: navOpen ? "0" : "-100%",
+                        transitionDelay: navOpen ? "0s" : "0s"
+                    }}>
 
+                    </div>
                 </div>
             </div>
         </div>
