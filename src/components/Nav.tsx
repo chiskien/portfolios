b3763c76a2befb63657893589133a4aa0de1c8ba
ChisKien <chiskien03214@gmail.com>
2023-02-06 10:18:07 +0700

    Styling using sass

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,6 +4,18 @@ export function Nav() {
     return (
         <div className={"nav"}>
             <h1>Navigation</h1>
+            <div className="nav-container">
+                <div className="logo">ChisKien</div>
+                <div className="menu-toggle">
+                      <div className="hambuger-box">
+                          <span className="line-top"></span>
+                          <span className="line-bottom"></span>
+                      </div>
+                </div>
+                <div className="nav-overlay">
+                    
+                </div>
+            </div>
         </div>
     )
 }
